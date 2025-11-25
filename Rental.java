1b2e37fb416d9b809df9c237e05b8ddd9cc9fb14
Gustavo pellá Bazanella <gustavo.pella.baza@gmail.com>
2025-11-25 19:15:17 -0300

    Commit 7

--- Rental.java
+++ Rental.java
@@ -1,4 +1,4 @@
-public class Rental {
+class Rental {
    private Movie _movie;
    private int _daysRented;
 
@@ -17,33 +17,28 @@ public class Rental {
 
    public double getCharge() {
       double result = 0;
-
       switch (getMovie().getPriceCode()) {
          case Movie.REGULAR:
             result += 2;
             if (getDaysRented() > 2)
                result += (getDaysRented() - 2) * 1.5;
             break;
-
          case Movie.NEW_RELEASE:
             result += getDaysRented() * 3;
             break;
-
          case Movie.CHILDRENS:
             result += 1.5;
             if (getDaysRented() > 3)
                result += (getDaysRented() - 3) * 1.5;
             break;
       }
-
       return result;
    }
 
    public int getFrequentRenterPoints() {
-      if (getMovie().getPriceCode() == Movie.NEW_RELEASE &&
-          getDaysRented() > 1) {
-         return 2;
-      }
+      //Adicionar o trecho de código extraído.
+      if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+         getDaysRented() > 1) return 2;
       return 1;
    }
 }
